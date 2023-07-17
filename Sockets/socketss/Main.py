5870aafa658e5fbb87f522241943dd2e06fc110c
Wesley Assis <88063740+WesleyAssiss@users.noreply.github.com>
2023-07-17 20:00:00 -0300

    Main.py

--- Sockets/socketss/Main.py
+++ Sockets/socketss/Main.py
@@ -1,12 +1,15 @@
+# Importação dos módulos/classes necessários
 from Servidor import Servidor
 from ClienteSensor import ClienteSensor
 from ClienteAtuador import ClienteAtuador
 
 def main():
+    # Criação das instâncias das classes Servidor, ClienteSensor e ClienteAtuador
     servidor = Servidor()
     cliente_sensor = ClienteSensor()
     cliente_atuador = ClienteAtuador()
 
+    # Inicialização e execução dos threads das classes Servidor, ClienteSensor e ClienteAtuador
     servidor.start()
     cliente_sensor.start()
     cliente_atuador.start()
